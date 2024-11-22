ff0a9423dfe19de4b0cd669af2e603ba3e4f78a6
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-22 17:32:53 +0100

    Add findByEmail method to UserRepository
    Enhanced UserRepository by adding a method to find a user by email. This allows more efficient querying of user data based on the email attribute.

--- back/src/main/java/com/mdd/back/Repository/UserRepository.java
+++ back/src/main/java/com/mdd/back/Repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.mdd.back.Model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    User findByEmail(String login);
 }
