36127e2286cc5abb938674d6aaac3e469b75b351
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-22 17:08:29 +0100

    Add Article model
    Introduce the Article model to represent articles in the database. This model includes fields for id, name, subscribers, and related articles, with appropriate JPA annotations for entity definition, relationships, and constraints.

---
+++ back/src/main/java/com/mdd/back/Model/Article.java
@@ -0,0 +1,31 @@
+package com.mdd.back.Model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "subscriptions")
+    private List<User> subscribers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "theme", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Article> articles = new ArrayList<>();
+
+
+}
