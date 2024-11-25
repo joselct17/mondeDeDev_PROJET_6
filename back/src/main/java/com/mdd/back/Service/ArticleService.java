6e4fa094bdcba68600abf6c1810f411d7bead825
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-25 18:02:57 +0100

    Refactor getAllArticles method to handle empty lists
    Convert Iterable returned by findAll() to List and check for empty lists before mapping entities to DTOs. This ensures the method handles cases where no articles are found and returns an empty list accordingly.

--- back/src/main/java/com/mdd/back/Service/ArticleService.java
+++ back/src/main/java/com/mdd/back/Service/ArticleService.java
@@ -8,8 +8,10 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @AllArgsConstructor
@@ -26,10 +28,25 @@ public class ArticleService {
     }
 
     public List<ArticleDto> getAllArticles() {
-        return articleRepository.findAll().stream()
+        // Récupérer tous les articles
+        Iterable<Article> articlesIterable = articleRepository.findAll();
+
+        // Convertir l'Iterable en List
+        List<Article> articles = StreamSupport.stream(articlesIterable.spliterator(), false)
+                .collect(Collectors.toList());
+
+        // Vérifier si la liste est vide
+        if (articles.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // Mapper les entités en DTOs
+        return articles.stream()
                 .map(article -> modelMapper.map(article, ArticleDto.class))
                 .collect(Collectors.toList());
     }
+
+
 
     public ArticleDto getArticleById(Long id) {
         return modelMapper.map(articleRepository.findById(id).orElse(null), ArticleDto.class);
