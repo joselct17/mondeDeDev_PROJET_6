ca8adda7a999cb216f4119b76ec767681bf0a4f8
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-25 09:32:01 +0100

    Add timestamp fields to User model
    Introduced `createdAt` and `updatedAt` fields in the `User` model to track entity creation and modification times. This enhances traceability and data integrity for user records.

--- back/src/main/java/com/mdd/back/Model/User.java
+++ back/src/main/java/com/mdd/back/Model/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +30,10 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Article> articles = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
     @ManyToMany
     @JoinTable(
