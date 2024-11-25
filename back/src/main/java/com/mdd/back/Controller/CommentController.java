38fe222b2c878a61d3be335605f6b968a20d8b54
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-25 09:31:17 +0100

    Add CommentController to handle comment-related endpoints
    This commit introduces a new CommentController class annotated with @RestController and @RequestMapping. The controller is set up to map requests to "/api/comment" endpoints, setting the foundation for managing comment operations.

---
+++ back/src/main/java/com/mdd/back/Controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.mdd.back.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+
+}
