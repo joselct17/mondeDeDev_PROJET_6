c91ada7074b501147bb7fa7df45b6a441769df4d
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-11-25 09:30:46 +0100

    Add content, datePosted, and relationships to Article
    Introduced new fields `content` and `datePosted` to the `Article` class for storing article text and posting date. Also added `user` relationship indicating the author and `comments` list for storing related comments. This improves article management and associations within the system.

--- back/src/main/java/com/mdd/back/Model/Article.java
+++ back/src/main/java/com/mdd/back/Model/Article.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +22,24 @@ public class Article {
     @Column(nullable = false, unique = true)
     private String name;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime datePosted;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @ManyToMany(mappedBy = "subscriptions")
     private List<User> subscribers = new ArrayList<>();
 
     @OneToMany(mappedBy = "theme", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Article> articles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 
 
 }
