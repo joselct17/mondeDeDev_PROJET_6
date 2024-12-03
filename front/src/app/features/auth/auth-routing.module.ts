ed217be8dd343eb15db613a0720d4b8bb6711c29
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 15:01:58 +0100

    Add landing page to auth routing module
    Introduce a new route for the LandingPageComponent as the default path in the auth-routing.module.ts file. This change sets the landing page as the main entry point for the authentication feature.

--- front/src/app/features/auth/auth-routing.module.ts
+++ front/src/app/features/auth/auth-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent }, // Page principale
   { title: 'Login', path: 'login', component: LoginComponent },
   { title: 'Register', path: 'register', component: RegisterComponent }
 ];
