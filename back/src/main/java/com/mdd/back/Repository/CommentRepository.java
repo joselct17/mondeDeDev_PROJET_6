c23c74415b0657847f6e9c177016c14dd29047d5
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:15:47 +0100

    Switch CommentRepository to JpaRepository and add query method.
    Replaced CrudRepository with JpaRepository to leverage JPA's advanced capabilities. Implemented a method to find comments by articleId, improving database interaction for related queries. This change enhances the maintainability and scalability of the codebase.

--- back/src/main/java/com/mdd/back/Repository/CommentRepository.java
+++ back/src/main/java/com/mdd/back/Repository/CommentRepository.java
@@ -1,10 +1,15 @@
 package com.mdd.back.Repository;
 
 import com.mdd.back.Model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Integer> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByArticleId(Long articleId);
 }
