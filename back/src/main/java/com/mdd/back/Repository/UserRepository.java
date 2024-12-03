653b04382149f8de81587bb619d05556c741d754
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:16:46 +0100

    Switch UserRepository to extend JpaRepository
    Migrating UserRepository from CrudRepository to JpaRepository enables more complex query capabilities and efficient operation handling. JpaRepository extends the functionality of CrudRepository by offering a rich set of functionalities for sorting, pagination, and customizable persistence logic, which can enhance our application's data management feature set.

--- back/src/main/java/com/mdd/back/Repository/UserRepository.java
+++ back/src/main/java/com/mdd/back/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.mdd.back.Repository;
 
 import com.mdd.back.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     User findByEmail(String login);
