a246c12b448550c2033c5305c496a7b5cdead36e
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:13:50 +0100

    Switch to JpaRepository in ArticleRepository.
    Replacing CrudRepository with JpaRepository enhances the repository functionality by providing additional JPA-specific methods. This change will help in achieving richer querying capabilities and improved data manipulation in our Article entity management.

--- back/src/main/java/com/mdd/back/Repository/ArticleRepository.java
+++ back/src/main/java/com/mdd/back/Repository/ArticleRepository.java
@@ -1,10 +1,11 @@
 package com.mdd.back.Repository;
 
 import com.mdd.back.Model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
