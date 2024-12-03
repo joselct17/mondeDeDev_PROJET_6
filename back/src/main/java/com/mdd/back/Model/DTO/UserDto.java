691dd0a7b98ef5a6366de94480585da28fd0b1ae
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:16:40 +0100

    Add timestamps and constructor to UserDto
    Introduced `created_at` and `updated_at` fields in `UserDto` for tracking timestamps. A constructor was added to initialize `UserDto` from a `User` object, enhancing object creation and data integrity.

--- back/src/main/java/com/mdd/back/Model/DTO/UserDto.java
+++ back/src/main/java/com/mdd/back/Model/DTO/UserDto.java
@@ -1,16 +1,26 @@
 package com.mdd.back.Model.DTO;
 
 
+import com.mdd.back.Model.User;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class UserDto {
 
     private String email;
-
     private String password;
-
-    private String confirmPassword;
-
     private String username;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public UserDto(User user) {
+        email = user.getEmail();
+        password = user.getPassword();
+        username = user.getUsername();
+        created_at = user.getCreatedAt();
+        updated_at = user.getUpdatedAt();
+    }
 }
