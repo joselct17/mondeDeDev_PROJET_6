737fec334198ad5f582c3017694d6903e194a61f
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:16:24 +0100

    Switch ThemeRepository to JpaRepository.
    This change updates the ThemeRepository interface to extend JpaRepository instead of CrudRepository. This allows for more advanced data access capabilities and additional methods provided by the JpaRepository interface.

--- back/src/main/java/com/mdd/back/Repository/ThemeRepository.java
+++ back/src/main/java/com/mdd/back/Repository/ThemeRepository.java
@@ -1,11 +1,12 @@
 package com.mdd.back.Repository;
 
 import com.mdd.back.Model.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ThemeRepository extends CrudRepository<Theme, Integer> {
+public interface ThemeRepository extends JpaRepository<Theme, Integer> {
     Theme findByName(String name);
 }
