eb3d096bbfbe300b27e7d9f817ce734d27c60250
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:13:56 +0100

    Simplify article retrieval in ArticleService
    Remove unnecessary conversion from Iterable to List by directly using the findAll method from articleRepository. This change reduces complexity and improves readability by eliminating the use of StreamSupport for this purpose.

--- back/src/main/java/com/mdd/back/Service/ArticleService.java
+++ back/src/main/java/com/mdd/back/Service/ArticleService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Service
 @AllArgsConstructor
@@ -29,23 +28,16 @@ public class ArticleService {
 
     public List<ArticleDto> getAllArticles() {
         // Récupérer tous les articles
-        Iterable<Article> articlesIterable = articleRepository.findAll();
-
-        // Convertir l'Iterable en List
-        List<Article> articles = StreamSupport.stream(articlesIterable.spliterator(), false)
-                .collect(Collectors.toList());
-
-        // Vérifier si la liste est vide
+        List<Article> articles = articleRepository.findAll();
+        // Vérifier si des articles sont présents
         if (articles.isEmpty()) {
             return Collections.emptyList();
         }
-
         // Mapper les entités en DTOs
         return articles.stream()
                 .map(article -> modelMapper.map(article, ArticleDto.class))
                 .collect(Collectors.toList());
     }
-
 
 
     public ArticleDto getArticleById(Long id) {
