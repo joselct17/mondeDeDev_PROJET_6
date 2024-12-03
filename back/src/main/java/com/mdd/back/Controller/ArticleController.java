891025541cc7baeddaab77742267a001f2639cd0
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-03 10:13:45 +0100

    Add logging to ArticleController for article creation
    Introduced Slf4j logging to ArticleController to enhance traceability and debugging by logging article creation events. The logger outputs an info-level message, providing visibility into the article creation process with article details.

--- back/src/main/java/com/mdd/back/Controller/ArticleController.java
+++ back/src/main/java/com/mdd/back/Controller/ArticleController.java
@@ -4,6 +4,7 @@ package com.mdd.back.Controller;
 import com.mdd.back.Model.DTO.ArticleDto;
 import com.mdd.back.Service.ArticleService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/articles")
 @AllArgsConstructor
+@Slf4j
 public class ArticleController {
 
     private final ArticleService articleService;
 
     @PostMapping
     public ResponseEntity<String> createArticle(@RequestBody ArticleDto articleDto) {
+        log.info("Creating article: {}", articleDto);
         articleService.createArticle(articleDto);
         return ResponseEntity.ok("Article created successfully");
     }
