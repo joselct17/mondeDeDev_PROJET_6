ea3ac07d4721345fafbe9985735ff422f50b13fc
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 16:40:02 +0100

    Add validation annotations to RegisterUserDto.
    The RegisterUserDto class now includes constraints to ensure that the username, email, and password fields are not left blank. Additionally, the email must be valid, and the password must be at least 8 characters long. These changes improve data integrity and user input validation.

--- back/src/main/java/com/mdd/back/Model/DTO/RegisterUserDto.java
+++ back/src/main/java/com/mdd/back/Model/DTO/RegisterUserDto.java
@@ -1,19 +1,23 @@
 package com.mdd.back.Model.DTO;
 
-import com.mdd.back.Model.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserDto {
 
+    @NotBlank(message = "Username is required")
     private String userName;
-    private String email;
-    private String password;
 
-    public RegisterUserDto(User users) {
-        this.userName = users.getUsername();
-        this.email = users.getEmail();
-        this.password = users.getPassword();
-    }
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is not valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
 
 }
