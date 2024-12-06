bb2c9db846af92e138b3ad3b7367d64cff0fe5d8
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 16:40:23 +0100

    Add fields and constructors to ThemeDto class
    Introduce `name`, `articles`, and `subscribers` fields to the `ThemeDto` class to store associated data efficiently. Additionally, add a `NoArgsConstructor` to enable easier instantiation of the objects. These changes enhance the functionality and usability of the `ThemeDto` class in handling related data.

--- back/src/main/java/com/mdd/back/Model/DTO/ThemeDto.java
+++ back/src/main/java/com/mdd/back/Model/DTO/ThemeDto.java
@@ -1,7 +1,17 @@
 package com.mdd.back.Model.DTO;
 
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class ThemeDto {
+    String name;
+    List<ArticleDto> articles;
+    List<SubscriberDto> subscribers;
+
 }
