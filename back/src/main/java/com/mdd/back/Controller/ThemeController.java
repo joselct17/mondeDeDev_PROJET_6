5049c6e727618996eb312dd5f22b8fbca75c2203
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 11:13:48 +0100

    Add theme subscription and unsubscription endpoints
    Introduced new endpoints to subscribe and unsubscribe users from themes using their IDs. These enhancements improve user interaction with themes by allowing dynamic subscription management, reflected through HTTP POST and DELETE methods.

--- back/src/main/java/com/mdd/back/Controller/ThemeController.java
+++ back/src/main/java/com/mdd/back/Controller/ThemeController.java
@@ -14,17 +14,34 @@ public class ThemeController {
 
     private final ThemeService themeService;
 
+    /**
+     * Retrieves all themes available in the system.
+     *
+     * @return ResponseEntity containing an Iterable of ThemeDto representing all available themes
+     */
     @GetMapping
     public ResponseEntity<Iterable<ThemeDto>> getThemes() {
         return ResponseEntity.ok(themeService.getAllThemes());
     }
 
+    /**
+     * Subscribes a user to a specific theme identified by the provided ID.
+     *
+     * @param id The ID of the theme to subscribe to
+     * @return ResponseEntity indicating the success of subscribing to the theme
+     */
     @PostMapping("/subscribe/{id}")
     public ResponseEntity<String> subscribeToTheme(@PathVariable Long id) {
         themeService.subscribeToTheme(id);
         return ResponseEntity.ok("Subscribed to theme successfully");
     }
 
+    /**
+     * Unsubscribes the user from a specific theme identified by the provided ID.
+     *
+     * @param id The ID of the theme to unsubscribe from
+     * @return ResponseEntity indicating the success of unsubscribing from the theme with a message "Unsubscribed from theme successfully"
+     */
     @DeleteMapping("/unsubscribe/{id}")
     public ResponseEntity<String> unsubscribeFromTheme(@PathVariable Long id) {
         themeService.unsubscribeFromTheme(id);
