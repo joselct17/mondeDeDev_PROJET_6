b1c59f2f1b0e6380d447ac273ccdbe632c1b5b53
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 19:47:43 +0100

    Add authentication handling to app component
    Integrate AuthService and SessionService to manage user sessions. Implement methods for automatic login and logout based on user state. Ensure user navigation on session state changes using Router.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {SessionService} from "./services/session.service";
+import {Router} from "@angular/router";
+import {AuthService} from "./features/auth/services/auth.service";
+import {Observable} from "rxjs";
+import {User} from "./interfaces/user.interface";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private sessionService: SessionService) {
+  }
+
+  public ngOnInit(): void {
+    this.autoLog();
+  }
+
+  public $isLogged(): Observable<boolean> {
+    return this.sessionService.$isLogged();
+  }
+
+  public logout(): void {
+    this.sessionService.logOut();
+    this.router.navigate([''])
+  }
+  public autoLog(): void {
+    this.authService.me().subscribe(
+      (user: User) => {
+        this.sessionService.logIn(user);
+      },
+      (_) => {
+        this.sessionService.logOut();
+      }
+    )
+  }
+
 }
