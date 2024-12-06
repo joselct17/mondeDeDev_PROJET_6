a444fe4d5d8525423d1a0e7524ceb5e5d5a8df12
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 19:00:58 +0100

    Refactor AuthService to use environment variables
    Updated the AuthService to leverage environment variables for API URLs, ensuring more flexible configuration. Modified endpoint paths to include '/auth' and added error handling in the login method to enhance robustness in case of connection failures.

--- front/src/app/features/auth/services/auth.service.ts
+++ front/src/app/features/auth/services/auth.service.ts
@@ -1,29 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {catchError, Observable, throwError} from 'rxjs';
 import { LoginRequest } from '../interfaces/loginRequest.interface';
 import { AuthSuccess  } from '../interfaces/authSuccess.interface';
 import { RegisterRequest } from '../interfaces/registerRequest.interface';
 import { User } from 'src/app/interfaces/user.interface';
+import {environment} from "../../../../environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private pathService = 'api/auth';
+  private pathService = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) { }
 
   public register(registerRequest: RegisterRequest): Observable<AuthSuccess> {
-    return this.httpClient.post<AuthSuccess>(`${this.pathService}/register`, registerRequest);
-  }
-
-  public login(loginRequest: LoginRequest): Observable<AuthSuccess> {
-    return this.httpClient.post<AuthSuccess>(`${this.pathService}/login`, loginRequest);
+    return this.httpClient.post<AuthSuccess>(`${this.pathService}/auth/register`, registerRequest);
   }
 
   public me(): Observable<User> {
-    return this.httpClient.get<User>(`${this.pathService}/me`);
+    return this.httpClient.get<User>(`${this.pathService}/auth/me`);
   }
+
+  public login(loginRequest: LoginRequest): Observable<AuthSuccess> {
+    return this.httpClient.post<AuthSuccess>(`${this.pathService}/auth/login`, loginRequest).pipe(
+      catchError(err => {
+        console.error('Erreur de connexion', err);
+        return throwError(() => new Error('Connexion échouée'));
+      })
+    );
+  }
+
 }
