7a79af5c58264a796ede6749903ce6c3f34f50d3
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 16:38:26 +0100

    Add NoArgsConstructor, AllArgsConstructor to ArticleDto
    Enhance ArticleDto by including NoArgsConstructor and AllArgsConstructor annotations to facilitate object creation. Rename the field 'title' to 'name', add a 'theme' field, and include a list of comments to better structure the article data. These changes improve the flexibility and extensibility of the data transfer object.

--- back/src/main/java/com/mdd/back/Model/DTO/ArticleDto.java
+++ back/src/main/java/com/mdd/back/Model/DTO/ArticleDto.java
@@ -1,10 +1,21 @@
 package com.mdd.back.Model.DTO;
 
 
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleDto {
-    private String title;
+    private String name;
     private String content;
+    private String theme;
+    private List<CommentResponseDto> comments;
+
 }
