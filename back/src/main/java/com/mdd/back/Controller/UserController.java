4b5ec624d76d95b400e2f97c512c3be1e7d169df
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 11:14:03 +0100

    Add user profile documentation to UserController methods
    Enhance the UserController by adding Javadoc comments to the methods responsible for retrieving and updating user profiles. This documentation improves code readability and assists developers in understanding the purpose and usage of these API endpoints.

--- back/src/main/java/com/mdd/back/Controller/UserController.java
+++ back/src/main/java/com/mdd/back/Controller/UserController.java
@@ -14,11 +14,22 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * Retrieve the user profile.
+     *
+     * @return ResponseEntity containing the user's profile information as UserProfileDto
+     */
     @GetMapping("/profile")
     public ResponseEntity<UserProfileDto> getUserProfile() {
         return ResponseEntity.ok(userService.getUserProfile());
     }
 
+    /**
+     * Updates the user profile with the provided data.
+     *
+     * @param profileDto The UserProfileDto object containing the updated user profile data
+     * @return ResponseEntity<String> indicating the success message for updating the profile
+     */
     @PutMapping("/profile")
     public ResponseEntity<String> updateUserProfile(@RequestBody UserProfileDto profileDto) {
         userService.updateUserProfile(profileDto);
