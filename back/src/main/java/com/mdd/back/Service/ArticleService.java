04bfab091658ef47ae3ee1e47250d8ef5daa8472
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 16:38:33 +0100

    Enhance ArticleService with theme and user data handling
    This commit updates the ArticleService to include user information and theme data when creating articles. It also maps article comments to CommentResponseDto in the getAllArticles method, improving the DTO to reflect theme and comment details accurately.

--- back/src/main/java/com/mdd/back/Service/ArticleService.java
+++ back/src/main/java/com/mdd/back/Service/ArticleService.java
@@ -3,11 +3,16 @@ package com.mdd.back.Service;
 
 import com.mdd.back.Model.Article;
 import com.mdd.back.Model.DTO.ArticleDto;
+import com.mdd.back.Model.DTO.CommentResponseDto;
+import com.mdd.back.Model.Theme;
+import com.mdd.back.Model.User;
 import com.mdd.back.Repository.ArticleRepository;
+import com.mdd.back.Repository.ThemeRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,12 +24,18 @@ public class ArticleService {
 
     private final ArticleRepository articleRepository;
     private final ModelMapper modelMapper;
+    private final ThemeRepository themeRepository;
 
 
-    public void createArticle(ArticleDto articleDto) {
+    public void createArticle(ArticleDto articleDto, User user) {
         Article article = new Article();
-        article.setName(articleDto.getTitle());
+        article.setUser(user);
+        article.setDatePosted(LocalDateTime.now());
+        article.setName(articleDto.getName());
         article.setContent(articleDto.getContent());
+        Theme theme = themeRepository.findByName(articleDto.getTheme());
+        article.setTheme(theme);
+        articleRepository.save(article);
     }
 
     /**
@@ -33,15 +44,26 @@ public class ArticleService {
      * @return List<ArticleDto> containing all articles as DTOs
      */
     public List<ArticleDto> getAllArticles() {
-        // Récupérer tous les articles
-        List<Article> articles = articleRepository.findAll();
-        // Vérifier si des articles sont présents
-        if (articles.isEmpty()) {
-            return Collections.emptyList();
-        }
-        // Mapper les entités en DTOs
-        return articles.stream()
-                .map(article -> modelMapper.map(article, ArticleDto.class))
+        return articleRepository.findAll()
+                .stream()
+                .map(article -> {
+                    ArticleDto articleDto = modelMapper.map(article, ArticleDto.class);
+
+                    articleDto.setTheme(article.getTheme().getName());
+                    // Mapper les commentaires
+                    articleDto.setComments(
+                            article.getComments().stream()
+                                    .map(comment -> {
+                                        CommentResponseDto commentDto = new CommentResponseDto();
+                                        commentDto.setContent(comment.getContent());
+                                        commentDto.setDatePosted(comment.getDatePosted());
+                                        commentDto.setUserName(comment.getUser().getUsername());
+                                        return commentDto;
+                                    })
+                                    .collect(Collectors.toList())
+                    );
+                    return articleDto;
+                })
                 .collect(Collectors.toList());
     }
 
