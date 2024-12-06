049ce60d9c08d66d7505776a8147a2843b83f2a2
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-06 16:39:39 +0100

    Add CommentResponseDto class to handle comment data
    Introduce CommentResponseDto with fields for content, date posted, and username. This class utilizes Lombok annotations to simplify boilerplate code and aid in data transfer operations concerning comments.

---
+++ back/src/main/java/com/mdd/back/Model/DTO/CommentResponseDto.java
@@ -0,0 +1,16 @@
+package com.mdd.back.Model.DTO;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class CommentResponseDto {
+
+    private String content;
+    private LocalDateTime datePosted;
+    private String userName;
+}
