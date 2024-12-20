7ddd6923898d6e8d20bd9fd9dd35943891a060c0
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-20 19:01:14 +0100

    rajout du profil de l'user

--- front/src/app/components/me/me.component.ts
+++ front/src/app/components/me/me.component.ts
@@ -1,26 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
+import { ThemeService } from 'src/app/features/theme/services/theme.service';
 import { User } from 'src/app/interfaces/user.interface';
+import { Theme } from 'src/app/features/theme/interfaces/theme.interface';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-me',
   templateUrl: './me.component.html',
-  styleUrls: ['./me.component.scss']
+  styleUrls: ['./me.component.scss'],
 })
 export class MeComponent implements OnInit {
-
   public user: User | undefined;
-
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private themeService: ThemeService,
+    private cd: ChangeDetectorRef // Inject ChangeDetectorRef
+  ) {}
 
   public ngOnInit(): void {
-    this.authService.me().subscribe(
-      (user: User) => this.user = user
-    )
+    // Charger les informations utilisateur
+    this.authService.me().subscribe((user: User) => {
+      // Ajoutez isSubscribed = true à chaque thème de la liste
+      this.user = {
+        ...user,
+        subscribedThemes: user.subscribedThemes.map((theme) => ({
+          ...theme,
+          isSubscribed: true // Tous les thèmes de cette liste sont déjà abonnés
+        }))
+      };
+
+      //Force Angular à vérifier les modifications
+      this.cd.detectChanges();
+    });
   }
 
-  public back() {
+  // Fonction de souscription / désabonnement
+  toggleSubscription(theme: Theme): void {
+    if (theme.isSubscribed) {
+      this.themeService.unsubscribe(theme.id).subscribe(
+        () => {
+          alert('Désabonné avec succès');
+          theme.isSubscribed = false; // Met à jour l'état local
+        },
+        (error) => {
+          alert('Erreur lors du désabonnement');
+        }
+      );
+    } else {
+      this.themeService.subscribe(theme.id).subscribe(
+        () => {
+          alert('Abonné avec succès');
+          theme.isSubscribed = true; // Met à jour l'état local
+        },
+        (error) => {
+          alert('Erreur lors de la souscription');
+        }
+      );
+    }
+  }
+
+  public saveProfile(): void {
+    if (this.user) {
+      this.authService.updateProfile(this.user).subscribe(
+        () => {
+          alert('Profil mis à jour avec succès');
+        },
+        () => {
+          alert('Erreur lors de la mise à jour du profil');
+        }
+      );
+    }
+  }
+
+  public back(): void {
     window.history.back();
   }
-
 }
