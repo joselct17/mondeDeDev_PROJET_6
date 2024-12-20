96e3b3f8cf4cb59dda0a3c1036703f0751b94fa8
joselct17 <62754339+joselct17@users.noreply.github.com>
2024-12-20 18:55:35 +0100

    amelioration du dto pour avoir les subscribedThemes de l'user

--- back/src/main/java/com/mdd/back/Model/DTO/UserDto.java
+++ back/src/main/java/com/mdd/back/Model/DTO/UserDto.java
@@ -2,11 +2,14 @@ package com.mdd.back.Model.DTO;
 
 
 
+import com.mdd.back.Model.Theme;
 import com.mdd.back.Model.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +20,7 @@ public class UserDto {
     private String username;
     private LocalDateTime created_at;
     private LocalDateTime updated_at;
+    private List<ThemeDto> subscribedThemes;
 
     public UserDto(User user) {
         this.email = user.getEmail();
@@ -24,5 +28,12 @@ public class UserDto {
         this.username = user.getUsername();
         this.created_at = user.getCreatedAt();
         this.updated_at = user.getUpdatedAt();
+
+        // Convertir les abonnements en ThemeDto
+        if (user.getSubscriptions() != null) {
+            this.subscribedThemes = user.getSubscriptions().stream()
+                    .map(ThemeDto::new)
+                    .collect(Collectors.toList());
+        }
     }
 }
