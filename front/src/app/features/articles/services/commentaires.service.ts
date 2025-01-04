aeff24b1586e44b5edca6b7b5cb47e4640f5e476
joselct17 <62754339+joselct17@users.noreply.github.com>
2025-01-04 17:44:53 +0100

    rajout du pathService

--- front/src/app/features/articles/services/commentaires.service.ts
+++ front/src/app/features/articles/services/commentaires.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommentaireRequest } from '../interfaces/api/commentaireRequest.interface';
 import { CommentaireResponse } from '../interfaces/api/commentaireResponse.interface';
+import {environment} from "../../../../environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentairesService {
 
-  private pathService = 'api/messages';
+  private pathService = `${environment.apiUrl}/comment`;
 
   constructor(private httpClient: HttpClient) { }
 
