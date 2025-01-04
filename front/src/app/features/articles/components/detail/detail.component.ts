4c26f0ca0420740cdba671eb42e9fcc61f8a08af
joselct17 <62754339+joselct17@users.noreply.github.com>
2025-01-04 15:41:20 +0100

    on envoi une interface comment

--- front/src/app/features/articles/components/detail/detail.component.ts
+++ front/src/app/features/articles/components/detail/detail.component.ts
@@ -15,15 +15,16 @@ import {SessionService} from "../../../../services/session.service";
 })
 export class ArticleDetailComponent implements OnInit {
   article: Article | null = null; // Contient les données de l'article
-  comments:  { username: string; content: string }[] = [];
+  comments: CommentaireResponse[] = [];
   newComment: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private articleService: ArticlesService,
     private commentaireService: CommentairesService,
-    private sessionService:SessionService
-  ) {}
+    private sessionService: SessionService
+  ) {
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id'); // Récupération de l'ID dans l'URL
@@ -33,7 +34,7 @@ export class ArticleDetailComponent implements OnInit {
   }
 
   loadArticleDetails(id: string): void {
-    this.articleService.detail(id).subscribe((article:Article) => {
+    this.articleService.detail(id).subscribe((article: Article) => {
       this.article = article;
       this.comments = article.comments || []; // Exemple pour: Article | null récupérer les commentaires
     });
@@ -47,17 +48,16 @@ export class ArticleDetailComponent implements OnInit {
     }
 
     const commentRequest: CommentaireRequest = {
-      article_id: this.article!.id, // L'ID de l'article en tant que article_id
-      user_id:this.sessionService.isLogged?this.sessionService.user!.id:0 ,
-      message: this.newComment,
+      article_id: this.article!.id, // ID de l'article
+      content: this.newComment.trim(), // Nettoyage du commentaire
     };
 
     this.commentaireService.send(commentRequest).subscribe(
       (response: CommentaireResponse) => {
         // Ajouter le commentaire localement après succès
         this.comments.push({
-          username: 'Vous', // Par exemple, le nom de l'utilisateur connecté
-          content: this.newComment,
+          userName: this.article?.author || 'Auteur inconnu',
+          content: this.newComment
         });
 
         // Réinitialiser le champ de commentaire
